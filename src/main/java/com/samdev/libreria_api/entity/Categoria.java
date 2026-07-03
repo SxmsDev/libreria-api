f2e19c25db8d5ab7398428d664a8073c7dd47c31
SxmsDev <samuelblancom@tecnocomfenalco.edu.co>
2026-07-02 22:35:56 -0500

    Crear entidad Categoria

--- src/main/java/com/samdev/libreria_api/entity/Categoria.java
+++ src/main/java/com/samdev/libreria_api/entity/Categoria.java
@@ -1,5 +1,49 @@
 package com.samdev.libreria_api.entity;
 
+import jakarta.persistence.*;
+
+@Entity
 public class Categoria {
 
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String nombre;
+
+    private String descripcion;
+
+    public Categoria() {
+
+    }
+
+    public Categoria(String nombre, String descripcion) {
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
 }
